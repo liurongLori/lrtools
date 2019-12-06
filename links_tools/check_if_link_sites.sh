84093f0cdc52f437f540f853f841690b8cdef994
liurong <lori_liurong@163.com>
2019-12-06 12:18:23 +0800

    check domain in domain table not in site table

--- links_tools/check_if_link_sites.sh
+++ links_tools/check_if_link_sites.sh
@@ -6,10 +6,11 @@ else
 fi
 for domain in `cat /tmp/check_if_link_sites`;
 do
-    links=`mysql -umingdatrade -ptrade@mingDA123 links -e "select * from site where site_domain like '%$domain%'" 2> /dev/null`;
+    links=`mysql -umingdatrade -ptrade@mingDA123 links -e "select * from domain where domain like '%$domain%'" 2> /dev/null`;
     link_sites=`mysql -umingdatrade -ptrade@mingDA123 link_sites -e "select * from link_sites where site like '%$domain%'" 2> /dev/null`;
-    #echo "mysql -umingdatrade -ptrade@mingDA123 links -e \"select * from site where site_domain like '%$domain%'\"";
+    #echo "mysql -umingdatrade -ptrade@mingDA123 links -e \"select * from domain where domain like '%$domain%'\"";
     #echo "mysql -umingdatrade -ptrade@mingDA123 link_sites -e \"select * from link_sites where site like '%$domain%'\"";
+    #echo $domain;
     no_renew=`cat /tmp/no_renew_domains | grep $domain`;
     if [[ -z $no_renew ]];
     then
