9014658d15e23cce73de54d28bd8ebac5ceb99b5
liurong <lori_liurong@163.com>
2020-03-09 14:44:39 +0800

    add tool about fix status of domains

---
+++ links_tools/fix_status_of_domain.py
@@ -0,0 +1,30 @@
+import traceback
+from datetime import datetime, timedelta
+from api.data_model import Domain
+from api.arg_parser import ManagedArgumentParser
+
+
+if __name__ == '__main__':
+    parser = ManagedArgumentParser()
+    parser.add_argument('-sd', '--source_domains', default='/tmp/no_renew_domains')
+    parser.set_defaults(draw_graph=False)
+    args = parser.parse_args()
+
+    with ManagedArgumentParser.api_by_args(args) as api:
+        with open(args.source_domains) as f:
+            source_domains = [d.strip() for d in f.readlines()]
+        for d in source_domains:
+            domain = Domain.get(api.session(), d)
+            if not domain:
+                continue
+            expire_time = domain.registered_timestamp + timedelta(days=365)
+            now_time = datetime.now()
+            expire_days = expire_time - now_time
+            if expire_days.days <= 0:
+                if domain.status != 'abandoned':
+                    print('%s expired at %s, update %s -> %s' %
+                        (d, domain.registered_timestamp, domain.status, 'abandoned'))
+                    domain.status = 'abandoned'
+                    if domain.site:
+                        domain.site.status = 'abandoned'
+
